f700a3c17a215c6712c9063ec039ce6b13e4e8c8
Nitin Yadav <nitinyadavsapla1@gmail.com>
2025-08-21 22:56:21 +0530

    Added default express body parser for req.body

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ const { ServerConfig } = require('./config/index.js');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extented: true }));
+
 app.use('/api', apiRoutes);
-
-
 
 app.listen(ServerConfig.PORT, () => {
     console.log(`Server running on Port : ${ServerConfig.PORT}`);
